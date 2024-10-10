b159ff14caf4e831c0e6bcd5df25827db1cc4100
IvanBermejoHidalgo <ibermejo@elpuig.xeill.net>
2024-10-10 19:06:33 +0200

    cuatro botones

--- color_picker/built/index.js
+++ color_picker/built/index.js
@@ -1,6 +1,23 @@
 "use strict";
-function ChangeColor() {
-    let boton = document.getElementById("change");
-    boton.addEventListener('click', () => {
-    });
+function switchBg(color) {
+    let chosenColor = "white";
+    switch (color) {
+        case "R":
+            chosenColor = "red";
+            break;
+        case "Y":
+            chosenColor = "yellow";
+            break;
+        case "G":
+            chosenColor = "green";
+            break;
+        case "B":
+            chosenColor = "blue";
+            break;
+        default:
+            break;
+    }
+    document.body.style.backgroundColor = chosenColor;
 }
+let redButton = document.getElementById("redButton");
+redButton.addEventListener("click", () => switchBg('R'));
--- color_picker/src/ts/index.ts
+++ color_picker/src/ts/index.ts
@@ -1,8 +1,26 @@
-function ChangeColor() {
-    let boton = document.getElementById("change")!;
+function switchBg(color: string) {
+    let chosenColor: string = "white";
+    switch (color) {
+        case "R":
+            chosenColor = "red";
+            break;
+        case "Y":
+            chosenColor = "yellow";
+            break;
+        case "G":
+            chosenColor = "green";
+            break;
+        case "B":
+            chosenColor = "blue";
+            break;
+        default:
+            break;    
 
-    boton.addEventListener('click', () => {
-        
-    })
+    }
+
+    document.body.style.backgroundColor = chosenColor;
 
 }
+
+let redButton: HTMLButtonElement = document.getElementById("redButton") as HTMLButtonElement;
+redButton.addEventListener("click", () => switchBg('R'));
